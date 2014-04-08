b2101e4800632e390a119d304f28ae86258cc446
Greg Horn <gregmainland@gmail.com>
2014-04-08 11:11:20 +0200

    convert let - in blocks to where blocks

--- Rand.hs
+++ Rand.hs
@@ -22,40 +22,41 @@ createParameters feasible gen minB maxB =
 
 -- Case Feasible problem
 createBoundsF :: Int -> Integer -> Integer -> (Param, Bounds)
-createBoundsF gen minB maxB =
-  let [ma,mb,mc,md,mlx,mux,mly,muy,gen'] = take 9 $ randomRs (minB,maxB) (mkStdGen gen) :: [Integer]
-      [a,b,c,d,lx,ux,ly,uy] = zipWith (\x y -> x*y) (map fromInteger [ma,mb,mc,md,mlx,mux,mly,muy]) (take 8 $ randoms (mkStdGen gen) :: [Float])
-      [lbx,ubx,lby,uby] = [(min ux lx), (max ux lx), (min uy ly), (max uy ly)]
-      [llx, uux, lly, uuy] = [(min (c*lbx) (c*ubx)), (max (c*lbx) (c*ubx)), (min (d*lby) (d*uby)),(max (d*lby) (d*uby))]
-      gen2 = mkStdGen (fromInteger gen' :: Int)
-      randlist = zipWith (*) (map fromInteger (randomRs (minB,maxB) gen2 :: [Integer])) (randoms gen2 :: [Float])
-      (lbg,ubg) = genGBoundsF randlist llx uux lly uuy
-  in ((Param a b c d), (Bounds lbx ubx lby uby lbg ubg))
+createBoundsF gen minB maxB = ((Param a b c d), (Bounds lbx ubx lby uby lbg ubg))
+  where
+    [ma,mb,mc,md,mlx,mux,mly,muy,gen'] = take 9 $ randomRs (minB,maxB) (mkStdGen gen) :: [Integer]
+    [a,b,c,d,lx,ux,ly,uy] = zipWith (\x y -> x*y) (map fromInteger [ma,mb,mc,md,mlx,mux,mly,muy]) (take 8 $ randoms (mkStdGen gen) :: [Float])
+    [lbx,ubx,lby,uby] = [(min ux lx), (max ux lx), (min uy ly), (max uy ly)]
+    [llx, uux, lly, uuy] = [(min (c*lbx) (c*ubx)), (max (c*lbx) (c*ubx)), (min (d*lby) (d*uby)),(max (d*lby) (d*uby))]
+    gen2 = mkStdGen (fromInteger gen' :: Int)
+    randlist = zipWith (*) (map fromInteger (randomRs (minB,maxB) gen2 :: [Integer])) (randoms gen2 :: [Float])
+    (lbg,ubg) = genGBoundsF randlist llx uux lly uuy
 
 
 genGBoundsF :: [Float] -> Float -> Float -> Float -> Float -> (Float,Float)
 genGBoundsF rlist llx uux lly uuy =
-  let ([lg,ug], rest) = splitAt 2 rlist in  -- Step 1
   if isPossible llx uux lly uuy lg
   then (min lg ug, max lg ug)
   else if isPossible llx uux lly uuy ug
        then (min lg ug, max lg ug)
        else genGBoundsF rest llx uux lly uuy  -- Step 2
+  where
+    ([lg,ug], rest) = splitAt 2 rlist  -- Step 1
 
 isPossible :: Float -> Float -> Float -> Float -> Float -> Bool         -- Step 1
 isPossible llx uux lly uuy lg = (lg > llx + lly) && (lg < uux + uuy)
 
 
 -- Case Unfeasible problem
-
 createBoundsU :: Int -> Integer -> Integer -> (Param, Bounds)
 createBoundsU gen minB maxB =
-  let ([a,b,c,d,ux,lx,uy,ly,ug,lg],rest) = splitAt 10 $ zipWith (*) (map fromInteger (randomRs (minB,maxB) (mkStdGen gen) :: [Integer])) (randoms (mkStdGen gen) :: [Float]) in
   if not $ testBounds lg ug lx ux ly uy then ((Param a b c d), (Bounds lx ux ly uy lg ug))
-  else let [llx, uux, lly, uuy] = [(min (c*lx) (c*ux)), (max (c*lx) (c*ux)), (min (d*ly) (d*uy)),(max (d*ly) (d*uy))] in
-  if (uux+uuy < lg) || (llx +lly > ug) then ((Param a b c d), (Bounds lx ux ly uy lg ug))
-  else let (lbg,ubg) = genGBoundsU rest llx uux lly uuy in
-  ((Param a b c d), (Bounds lx ux ly uy lbg ubg))
+  else if (uux+uuy < lg) || (llx +lly > ug) then ((Param a b c d), (Bounds lx ux ly uy lg ug))
+       else  ((Param a b c d), (Bounds lx ux ly uy lbg ubg))
+  where
+    ([a,b,c,d,ux,lx,uy,ly,ug,lg],rest) = splitAt 10 $ zipWith (*) (map fromInteger (randomRs (minB,maxB) (mkStdGen gen) :: [Integer])) (randoms (mkStdGen gen) :: [Float])
+    [llx, uux, lly, uuy] = [(min (c*lx) (c*ux)), (max (c*lx) (c*ux)), (min (d*ly) (d*uy)),(max (d*ly) (d*uy))]
+    (lbg,ubg) = genGBoundsU rest llx uux lly uuy
 
 testBounds :: Float -> Float -> Float -> Float -> Float -> Float -> Bool -- Step 1
 testBounds lbg ubg lbx1 ubx1 lbx2 ubx2
@@ -67,10 +68,11 @@ testBounds lbg ubg lbx1 ubx1 lbx2 ubx2
 
 genGBoundsU :: [Float] -> Float -> Float -> Float -> Float -> (Float,Float)
 genGBoundsU rlist llx uux lly uuy =
-  let ([lg,ug], rest) = splitAt 2 rlist in
   if lg > ug then (lg,ug) -- Step 1
   else
     if isNotPossible llx uux lly uuy lg ug then (lg,ug) else genGBoundsF rest llx uux lly uuy
+  where
+    ([lg,ug], rest) = splitAt 2 rlist
 
 
 isNotPossible :: Float -> Float -> Float -> Float -> Float -> Float -> Bool -- Step 2
