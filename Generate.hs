11d5d475e3cd40af8c65059657aa47789eb8374b
philippe-mon <philippe-mon@users.noreply.github.com>
2014-04-10 15:21:43 +0200

    Only one kind of problem
    The user chooses if all the problems or feasible or not

--- Generate.hs
+++ Generate.hs
@@ -10,23 +10,23 @@ main = do
   gen <- getStdGen
   putStrLn "How many problems to generate ?"
   number <- getLine
+  putStrLn "Feasible [True] or Unfeasible [False] ?"
+  feas <- getLine
   let n = read number
-  generateProblem gen n
+      feasible = read feas
+  generateProblem gen feasible n
 
 
-generateProblem :: StdGen -> Int -> IO ()
-generateProblem gen n = do
-  let (feasible, gen2) = random gen :: (Bool, StdGen)
-      (bd, gen3) = random gen2 :: (Integer, StdGen)
+generateProblem :: StdGen -> Bool -> Int -> IO ()
+generateProblem gen feasible n = do
+  let (bd, gen3) = random gen :: (Integer, StdGen)
       (g, gen4) = random gen3 :: (Int, StdGen)
       (param, bounds) = R.createParameters feasible g (- 200) (200)
       prob = GLPK.Minimize [R.a param, R.b param]
       constr1 = GLPK.Dense [ [R.c param, R.d param] GLPK.:=>: R.getlbg bounds
                       , [R.c param, R.d param] GLPK.:<=: R.getubg bounds
                       ]
-  putStr $ show feasible ++ "   ->   "
   putStrLn.show $ GLPK.simplex prob constr1 [1 GLPK.:&: (R.getlbx1 bounds, R.getubx1 bounds), 2 GLPK.:&: (R.getlbx2 bounds, R.getubx2 bounds)] 
-  --putStrLn $ show param ++ show bounds
   if n > 1
-  then generateProblem gen4 (n-1)
+  then generateProblem gen4 feasible (n-1)
   else return ()
