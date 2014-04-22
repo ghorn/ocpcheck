cc4384a6c6368da40c6bc8aa8288f29950c51fb8
philippe-mon <philippe-mon@users.noreply.github.com>
2014-04-22 10:43:09 +0200

    With initial conditions

--- LPGenerate.hs
+++ LPGenerate.hs
@@ -18,7 +18,7 @@ type ParamCst = [Double]
 type BoundsSpace = [(Double,Double)]
 type BoundsCst = [(Double,Double)]
 
-generateProblem :: Integer -> Bool -> Int -> Int -> (ParamOpt, ParamCst, BoundsSpace, BoundsCst)
+generateProblem :: Integer -> Bool -> Int -> Int -> ([Double], ParamOpt, ParamCst, BoundsSpace, BoundsCst)
 generateProblem gen feasible n m =
   if feasible
   then generateProblemSF gen n m minB maxB
@@ -27,24 +27,24 @@ generateProblem gen feasible n m =
     maxB = 3000
     minB = -3000
 
-generateProblemSF :: Integer -> Int -> Int -> Integer -> Integer -> (ParamOpt, ParamCst, BoundsSpace, BoundsCst)
-generateProblemSF gen n m minB maxB = (opt, pcst, bsp, bcst)
+generateProblemSF :: Integer -> Int -> Int -> Integer -> Integer -> ([Double], ParamOpt, ParamCst, BoundsSpace, BoundsCst)
+generateProblemSF gen n m minB maxB = (x0, opt, pcst, bsp, bcst)
   where
   -- Generate parameters of the objective function
     gen':mopt = take (n+1) $ randomRs (minB, maxB) (mkStdGen (fromInteger gen :: Int)) :: [Integer]  
-    gen1 = if gen' == gen then (gen'+1) else gen'
+    gen1 = if gen' == gen then gen'+1 else gen'
     opt = zipWith (*) 
                   (map fromInteger mopt) 
                   (take n $ randoms (mkStdGen (fromInteger gen :: Int)) :: [Double])
   -- Generate parameters of the matrix A
     gen2':mpcst = take (n*m+1) $ randomRs (minB, maxB) (mkStdGen (fromInteger gen1 :: Int)) :: [Integer]  
-    gen2 = if gen2' == gen1 then (gen2'+1) else gen2'
+    gen2 = if gen2' == gen1 then gen2'+1 else gen2'
     pcst = zipWith (*) 
                   (map fromInteger mpcst)
                   (take (m*n) $ randoms (mkStdGen (fromInteger gen1 :: Int)) :: [Double])
   -- Generate possible solution
     gen3':msol = take (n+1) $ randomRs (minB, maxB) (mkStdGen (fromInteger gen2 :: Int)) :: [Integer]  
-    gen3 = if gen3' == gen2 then (gen3'+1) else gen3'
+    gen3 = if gen3' == gen2 then gen3'+1 else gen3'
     sol = zipWith (*) 
                   (map fromInteger msol)
                   (take (m*n) $ randoms (mkStdGen (fromInteger gen2 :: Int)) :: [Double])
@@ -59,6 +59,10 @@ generateProblemSF gen n m minB maxB = (opt, pcst, bsp, bcst)
     bcst = zipWith (\v g -> (fst $ randomR (v - great v, v - epsilon v) (mkStdGen g), fst $ randomR (v + epsilon v,v + great v) (mkStdGen g)))
                    asol
                    generator2
+  -- Generate starting point x0
+    x0 = zipWith (\(x,y) g -> fst $ randomR (x,y) (mkStdGen g) :: Double)
+                 bsp
+                 generator1
 
 
 epsilon :: Double -> Double
@@ -77,8 +81,8 @@ mult n m a v
 
 
 --Case Unfeasible Problem
-generateProblemU :: Integer -> Int -> Int -> Integer -> Integer -> (ParamOpt, ParamCst, BoundsSpace, BoundsCst)
-generateProblemU gen n m minB maxB = (opt, pcst, bsp, bcst)
+generateProblemU :: Integer -> Int -> Int -> Integer -> Integer -> ([Double], ParamOpt, ParamCst, BoundsSpace, BoundsCst)
+generateProblemU gen n m minB maxB = (x0, opt, pcst, bsp, bcst)
   where
   -- Generate parameters of the objective function
     gen1:mopt = take (n+1) $ randomRs (minB, maxB) (mkStdGen (fromInteger gen :: Int)) :: [Integer]  
@@ -104,11 +108,10 @@ generateProblemU gen n m minB maxB = (opt, pcst, bsp, bcst)
                   bsp2
   -- Generate the constraints bounds
     cstbounds = zip (createLowerBounds n m bsp pcst)
-                    (createUpperBounds n m bsp pcst)
-    gen5:mubcst = take (m+1) $ randomRs (minB, maxB) (mkStdGen (fromInteger gen4 :: Int)) :: [Integer]  
-    (constU, gen6) = randomR (1,m) (mkStdGen (fromInteger gen5 :: Int)) :: (Int, StdGen)
+                    (createUpperBounds n m bsp pcst)  
+    (constU, gen6) = randomR (1,m) (mkStdGen (fromInteger gen4 :: Int)) :: (Int, StdGen)
     (up, gen7) = random gen6 :: (Bool, StdGen)
-    generators = take (2*m) $ randoms gen7 :: [Int]
+    generators = take (2*m+n) $ randoms gen7 :: [Int]
     (genl, genu) = splitAt m generators
     listconst = take m [constU, (constU-1)..]
     genlb = zip listconst genl
@@ -138,6 +141,10 @@ generateProblemU gen n m minB maxB = (opt, pcst, bsp, bcst)
     bcst = zipWith (\x y -> (min x y, max x y))
                   lbcst
                   ubcst
+    -- Generate starting point x0
+    x0 = zipWith (\(x,y) g -> fst $ randomR (x,y) (mkStdGen g) :: Double)
+                 bsp
+                 generators
 
 
 createLowerBounds :: Int -> Int -> BoundsSpace -> ParamCst -> [Double]
